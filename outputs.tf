63969e1a13ae3a516b81dacd31e1c4e26861016c
bin <46985611+hwangbin@users.noreply.github.com>
2026-04-25 18:25:11 +0000

    add secure s3 baseline resources

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "selected_aws_region" {
   description = "The AWS region used by this project"
   value       = var.aws_region
 }
+
+output "s3_bucket_name" {
+  description = "Name of the secure S3 bucket"
+  value       = aws_s3_bucket.secure_bucket.bucket
+}
+
+output "s3_bucket_arn" {
+  description = "ARN of the secure S3 bucket"
+  value       = aws_s3_bucket.secure_bucket.arn
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,15 @@ variable "aws_region" {
   type        = string
   default     = "ap-northeast-1"
 }
+
+variable "project_name" {
+  description = "Project name used for resource naming"
+  type        = string
+  default     = "cloud-security-baseline"
+}
+
+variable "bucket_name" {
+  description = "Globally unique S3 bucket name"
+  type        = string
+  default     = "hwangbin-cloud-security-baseline-demo"
+}
